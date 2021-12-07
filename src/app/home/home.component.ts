6208d7399f6029f567ac08f7da9cafe701ccc0df
Donovan Allen <dallen.928@gmail.com>
2021-12-07 14:59:18 -0600

    ports over homepage animation code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild("marqueeItem", { static: false }) marqueeItem: any;
 
-  constructor() { }
+  name = "Donovan Allen";
+  location = "Brooklyn, NY";
+  list = [
+    "full-stack web developer",
+    "creator",
+    "athlete",
+    "musician",
+    "UX designer",
+  ];
+  typed = "";
+  el: any;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.el = this.marqueeItem.nativeElement;
+    console.log(this.el);
+    this.startType(this.getRandomItem(), 0);
   }
 
+  startType(item: any, index: any) {
+    if (index < item.length) {
+      this.typed += item.charAt(index);
+      this.el.innerHTML = this.typed;
+      index++;
+      setTimeout(() => {
+        this.startType(item, index);
+      }, 50);
+    } else {
+      setTimeout(() => {
+        this.el.classList.add("highlight");
+      }, 4000);
+
+      setTimeout(() => {
+        this.el.classList.remove("highlight");
+        this.typed = "";
+        this.el.innerHTML = this.typed;
+        this.startType(this.getRandomItem(), 0);
+      }, 5000);
+    }
+  }
+
+  getRandomItem() {
+    const index = Math.floor(Math.random() * this.list.length);
+    return this.list[index];
+  }
 }
